519ac51061d4cb8ec48f794ee7f6a5d4918b748c
raparsalan <raparsalan00@upi.edu>
2025-06-13 01:06:03 +0700

    update response

--- actionLogging/views.py
+++ actionLogging/views.py
@@ -24,7 +24,7 @@ class LogUserActionView(APIView):
         serializer = UserActionLogSerializer(user_log)
         return Response({
             "message": "Log created successfully",
-            "id": str(user_log.idLog),  # ini bagian penting untuk PHP
+            "id": str(user_log),  # ini bagian penting untuk PHP
             "data": serializer.data
         }, status=status.HTTP_200_OK)
     
