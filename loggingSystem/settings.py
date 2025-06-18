83b3d3166dce7f552634f323495602f65e4ba17d
raparsalan <raparsalan00@upi.edu>
2025-06-18 18:21:26 +0700

    update req.txt

--- loggingSystem/settings.py
+++ loggingSystem/settings.py
@@ -43,15 +43,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'actionLogging',
     'loggingAnalytics',
-    'corsheaders',
-]
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500", 
-    "http://localhost:5500", 
-    "https://fasilitaskema.site", 
 ]
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
